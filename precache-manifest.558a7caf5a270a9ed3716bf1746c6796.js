74cb910dbc48fa25201e06a742198da1266033e2
Vlad Shum <shumanskyivlad@gmail.com>
2019-11-26 02:04:42 +0200

    Updates

---
+++ precache-manifest.558a7caf5a270a9ed3716bf1746c6796.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60ade6adf2ee5eb6066b",
+    "url": "//Magistr/static/js/runtime~main.86ac3668.js"
+  },
+  {
+    "revision": "769e2ce337422bbf127f",
+    "url": "//Magistr/static/js/main.623774c4.chunk.js"
+  },
+  {
+    "revision": "beb99d74a6efae27311e",
+    "url": "//Magistr/static/js/2.14e48828.chunk.js"
+  },
+  {
+    "revision": "769e2ce337422bbf127f",
+    "url": "//Magistr/static/css/main.9ae59d05.chunk.css"
+  },
+  {
+    "revision": "7f855cab6d565ca07de2b6e3a09ab1e0",
+    "url": "//Magistr/index.html"
+  }
+];
